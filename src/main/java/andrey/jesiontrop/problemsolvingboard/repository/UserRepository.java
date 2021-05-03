0b9fbc550e3bb83afe4b5d347472b6d34a48c95c
Jusepa <andrey.ru754@gmail.com>
2021-05-04 02:17:10 +0300

    feat: create new function in UserRepository

--- src/main/java/andrey/jesiontrop/problemsolvingboard/repository/UserRepository.java
+++ src/main/java/andrey/jesiontrop/problemsolvingboard/repository/UserRepository.java
@@ -2,7 +2,9 @@ package andrey.jesiontrop.problemsolvingboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import andrey.jesiontrop.problemsolvingboard.model.Leader;
 import andrey.jesiontrop.problemsolvingboard.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
