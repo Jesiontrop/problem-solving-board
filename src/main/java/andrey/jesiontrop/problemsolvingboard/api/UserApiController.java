454f20d8f52ec6205a0e890b038e30b85f57f189
Jusepa <andrey.ru754@gmail.com>
2021-05-05 20:14:59 +0300

    feat: create UserApiController

---
+++ src/main/java/andrey/jesiontrop/problemsolvingboard/api/UserApiController.java
@@ -0,0 +1,45 @@
+package andrey.jesiontrop.problemsolvingboard.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.server.ExposesResourceFor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import andrey.jesiontrop.problemsolvingboard.model.User;
+import andrey.jesiontrop.problemsolvingboard.repository.UserRepository;
+import andrey.jesiontrop.problemsolvingboard.security.RegistrationForm;
+import lombok.extern.slf4j.Slf4j;
+
+@RepositoryRestController
+@CrossOrigin("*")
+@Slf4j
+public class UserApiController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserApiController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping(path="/users")
+    @ResponseBody
+    public ResponseEntity<User> registration(@RequestBody RegistrationForm form) {
+        log.debug(form.toString());
+        log.debug(form.toUser(passwordEncoder).toString());
+        log.debug("registation");
+        User user = userRepository.save(form.toUser(passwordEncoder));
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+}
