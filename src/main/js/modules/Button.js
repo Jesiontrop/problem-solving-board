23e0ae89c7ae58eb9bdab55d9b2bf0033558c583
Jusepa <andrey.ru754@gmail.com>
2021-05-09 20:13:57 +0300

    feat: add new version of button
    Also clean up code of button object

--- src/main/js/modules/Button.js
+++ src/main/js/modules/Button.js
@@ -2,21 +2,14 @@ import React from "react";
 import styles from "../styles/Button.module.scss";
 
 const Button = (props) => {
-    let text = "Text";
-    if (props.text)
-        text = props.text;
+    const text = props.text ? props.text : "Button";
 
-    let version = "1";
-    if (props.version)
-        version = props.version;
-    const styleVersion = "button-" + version;
+    const version = props.version ? props.version : "b-1"
 
-    let onCustomClick = function () {};
-    if (props.onClick)
-        onCustomClick = props.onClick;
+    const onCustomClick = props.onClick ? props.onClick : function () {};
 
     return (
-        <button className={styles[styleVersion]} style={props.style} onClick={onCustomClick}>
+        <button className={styles[version]} style={props.style} onClick={onCustomClick}>
             <p className={styles.text}>{text}</p>
         </button>
     );
