375aa21a30a10ac702e9c222074df79ceec99bc3
Jusepa <andrey.ru754@gmail.com>
2021-05-09 20:26:14 +0300

    feat: add new object Header

---
+++ src/main/js/modules/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Button} from "./Button";
+import style from "../styles/Header.module.scss"
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.headline = props.headline;
+        this.status = props.status;
+        this.buttonText = this.status ? "Профиль" : "Войти";
+        this.hideHomeButton = props.hideHomeButton;
+        this.styleHomeButton = this.hideHomeButton ? {display : "none"} : {};
+    }
+
+    render() {
+        return (
+            <header>
+                <Button version="b-2" style={this.styleHomeButton} text="ДРП"/>
+                <div className={style.text}>
+                    {this.headline}
+                </div>
+                <Button version="b-2" text={this.buttonText}/>
+            </header>
+        );
+    }
+}
+
+export {Header}
