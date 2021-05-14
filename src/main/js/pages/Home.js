ea901f5632b9ce2628e06e761a59620325b5a3ed
Jusepa <andrey.ru754@gmail.com>
2021-05-14 23:52:13 +0300

    refactor: clean code

--- src/main/js/pages/Home.js
+++ src/main/js/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Button} from "../modules/Button";
 import {Header} from "../modules/Header";
 import style from "../styles/Home.module.scss"
-import {Link, NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 class Home extends React.Component {
     constructor(props) {
