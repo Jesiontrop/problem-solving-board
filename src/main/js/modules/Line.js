066f3c12f3c52a5933e83f8b67f5f92c668e6805
Jusepa <andrey.ru754@gmail.com>
2021-05-18 22:59:48 +0300

    feat: add Line module

---
+++ src/main/js/modules/Line.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "../styles/Line.module.scss";
+import {InfoField} from "./Field";
+
+const Line = (props) => {
+    return (
+        <div className={style.line}>
+            <div className={style.text}>{props.subtitle}</div>
+            <InfoField className="f-2" text={props.text} style={props.infoStyle}/>
+        </div>
+    );
+}
+
+export {Line}
