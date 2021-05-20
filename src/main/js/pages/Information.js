78dd862db30fb1736ea60bf987f4ef3d47a743f9
Andrey Barkov <andrey.ru754@gmail.com>
2021-05-20 14:04:16 +0300

    feat: include api into information

--- src/main/js/pages/Information.js
+++ src/main/js/pages/Information.js
@@ -1,10 +1,27 @@
 import React from "react";
 import styles from "../styles/Information.module.scss";
+import client from "../client";
 import {Button} from "../modules/Button";
 import {Line} from "../modules/Line";
 import {Header} from "../modules/Header";
 
 class Information extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {board: {}};
+    }
+
+    componentDidMount() {
+        let path = "/api/viewBoards/" + this.props.match.params.id;
+        console.debug(path);
+        client({method: "GET", path: path }).done(response => {
+            this.setState({
+                board: response.entity
+            });
+            console.debug(response.entity);
+        });
+    }
 
     render() {
         return(
@@ -12,18 +29,18 @@ class Information extends React.Component {
                 <Header headline="Подробная информация о проблеме"/>
                 <main className={styles.informationPage}>
                     <Button className="b-3" text="Вернуться назад"/>
-                    <Line subtitle="Регистарционный номер"/>
-                    <Line subtitle="Время и дата регистрации"/>
-                    <Line subtitle="Участок"/>
-                    <Line subtitle="Проблема"/>
-                    <Line subtitle="Уровень риска"/>
-                    <Line subtitle="Предлагаемое решение"/>
-                    <Line subtitle="Ответственный за решение"/>
-                    <Line subtitle="Плановая дата решения проблемы"/>
-                    <Line subtitle="Фактическая дата решения проблемы"/>
-                    <Line subtitle="Уровень решения проблемы"/>
-                    <Line subtitle="Статус решения проблемы"/>
-                    <Line subtitle="Ответственное ФН за решение проблемы"/>
+                    <Line subtitle="Регистарционный номер" text={this.state.board.id}/>
+                    <Line subtitle="Время и дата регистрации" text={this.state.board.registrationDate}/>
+                    <Line subtitle="Участок" text={this.state.board.areaName}/>
+                    <Line subtitle="Проблема" text={this.state.board.problem}/>
+                    <Line subtitle="Уровень риска" text={this.state.board.riskLevelName}/>
+                    <Line subtitle="Предлагаемое решение" text={this.state.board.proposedSolution}/>
+                    <Line subtitle="Ответственный за решение" text={this.state.board.responsibleName}/>
+                    <Line subtitle="Плановая дата решения проблемы" text={this.state.board.plannedDate}/>
+                    <Line subtitle="Фактическая дата решения проблемы" text={this.state.board.actualDate}/>
+                    <Line subtitle="Уровень решения проблемы" text={this.state.board.solvingLevelName}/>
+                    <Line subtitle="Статус решения проблемы" text={this.state.board.resolutionStatusName}/>
+                    <Line subtitle="Ответственное ФН за решение проблемы" text={this.state.board.fmResponsibleName}/>
 
                 </main>
             </div>
