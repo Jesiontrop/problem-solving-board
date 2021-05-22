c0d7e8917a28f686741a33a6705c57a78e86aad9
Jusepa <andrey.ru754@gmail.com>
2021-05-22 18:13:36 +0300

    feat: include Overlay to Header

--- src/main/js/modules/Header.js
+++ src/main/js/modules/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
-import {Button} from "./Button";
+import { Button } from "./Button";
+import { Overlay } from "./Overlay";
 import style from "../styles/Header.module.scss"
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Header extends React.Component {
     constructor(props) {
@@ -10,20 +11,31 @@ class Header extends React.Component {
         this.status = props.status;
         this.buttonText = this.status ? "Профиль" : "Войти";
         this.hideHomeButton = props.hideHomeButton;
-        this.styleHomeButton = this.hideHomeButton ? {display : "none"} : {};
+        this.styleHomeButton = this.hideHomeButton ? { display: "none" } : {};
+        this.onShowOverlay = this.onShowOverlay.bind(this);
+        this.overlay = React.createRef();
+    }
+
+    onShowOverlay() {
+        // document.getElementById("overlay").style.display = "flex";
+        this.overlay.current.show();
     }
 
     render() {
         return (
-            <header className={style.header}>
-                <Link to="/" style={{textDecoration: 'none'}}><Button className="b-2" style={this.styleHomeButton} text="ДРП"/></Link>
-                <div className={style.text}>
-                    {this.headline}
-                </div>
-                <Button className="b-2" text={this.buttonText}/>
-            </header>
+            <div>
+                <header className={style.header}>
+                    <Link to="/" style={{ textDecoration: 'none' }}><Button className="b-2" style={this.styleHomeButton} text="ДРП" /></Link>
+                    <div className={style.text}>
+                        {this.headline}
+                    </div>
+                    <Button className="b-2" text={this.buttonText} onClick={this.onShowOverlay} />
+                </header>
+                <Overlay ref={this.overlay}/>
+            </div>
+
         );
     }
 }
 
-export {Header}
+export { Header }
