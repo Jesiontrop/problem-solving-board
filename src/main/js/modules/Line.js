37c64a5e2584c403c86f408f2c23aa5323ba7f30
Jusepa <andrey.ru754@gmail.com>
2021-05-30 20:30:45 +0300

    Update Line.js

--- src/main/js/modules/Line.js
+++ src/main/js/modules/Line.js
@@ -1,17 +1,26 @@
 import React from "react";
 import style from "../styles/Line.module.scss";
-import { EditField, InfoField, SelectField } from "./Field";
+import { EditField, InfoField, SelectField, TextareaField } from "./Field";
 
 const Line = (props) => {
     return (
         <div className={style.line}>
             <div className={style.text}>{props.subtitle}</div>
             {props.editable
-                ? <EditField className="f-2" text={props.text} style={props.infoStyle} />
-                : <InfoField className="f-2" text={props.text} style={props.infoStyle} />
+                ? <TextareaField id={props.id} className="f-2" text={props.text} style={props.infoStyle} />
+                : <InfoField id={props.id} className="f-2" text={props.text} style={props.infoStyle} />
             }
         </div>
     );
 }
 
-export { Line }
+const SelectLine = (props) => {
+    return (
+        <div className={style.line}>
+            <div className={style.text}>{props.subtitle}</div>
+            <SelectField id={props.id} items={props.items} className="f-2"/>
+        </div>
+    );
+}
+
+export { Line, SelectLine }
