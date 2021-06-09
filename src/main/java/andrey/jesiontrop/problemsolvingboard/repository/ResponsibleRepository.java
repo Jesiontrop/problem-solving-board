28ec7fa7c9673b03fdc21d530c2df7f3f5e336c4
Jusepa <andrey.ru754@gmail.com>
2021-06-09 20:41:58 +0300

    Update ResponsibleRepository.java

--- src/main/java/andrey/jesiontrop/problemsolvingboard/repository/ResponsibleRepository.java
+++ src/main/java/andrey/jesiontrop/problemsolvingboard/repository/ResponsibleRepository.java
@@ -1,8 +1,12 @@
 package andrey.jesiontrop.problemsolvingboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import andrey.jesiontrop.problemsolvingboard.model.Responsible;
+import andrey.jesiontrop.problemsolvingboard.model.User;
 
 public interface ResponsibleRepository extends JpaRepository<Responsible, Long> {
+
+    Responsible findByUserId(@Param("userId") Long userId);
 }
